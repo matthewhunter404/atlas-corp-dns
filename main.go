85271baebd3bcf34d33034da3dcee62f42e5cca6
Matthew Hunter <matthewhunter404@gmail.com>
2022-10-24 16:29:25 +0200

    Set config values via ENV variables

--- main.go
+++ main.go
@@ -4,14 +4,25 @@ import (
 	"fmt"
 	"go-atlas-corp/domain/sector"
 	"go-atlas-corp/server"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
-
-const SectorID = 1
 
 func main() {
 	fmt.Println("Server starting...")
-	sector := sector.New(uint(SectorID))
+	sectorID, err := strconv.Atoi(os.Getenv("atlas_dns_sector_id"))
+	if err != nil {
+		log.Fatalf("Invalid sector ID env variable detected, error: %v", err)
+	}
+	os.Getenv("atlas_dns_port")
+	port, err := strconv.Atoi(os.Getenv("atlas_dns_port"))
+	if err != nil {
+		log.Fatalf("Invalid port env variable detected, error: %v", err)
+	}
+
+	sector := sector.New(uint(sectorID))
 	newServer := server.New(sector)
-	http.ListenAndServe(fmt.Sprintf(":%d", 3000), newServer.Router())
+	http.ListenAndServe(fmt.Sprintf(":%d", port), newServer.Router())
 }
